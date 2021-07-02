4f3ca31d975151b6d4e595bc2cecc7053cdf7cdd
dimitrivitulin <79697501+dimitrivitulin@users.noreply.github.com>
2021-07-02 07:25:58 +0200

    Delete mycave directory

--- mycave/assets/js/script.js
+++
@@ -1,6 +0,0 @@
-const effacerVins = document.querySelectorAll(".effacer-vin");
-for (const effacerVin of effacerVins) {
-  effacerVin.addEventListener("click", function () {
-    console.log(effacerVin);
-  });
-}
